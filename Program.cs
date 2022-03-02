1d02a1012654dcc04cadd53d478cab899c1eee1a
SajeevDarshanapu <67383521+SajeevDarshanapu@users.noreply.github.com>
2022-03-02 15:44:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Enumerations
+{
+    public class BSection
+    {
+        enum ECE
+        {
+            RangaSai=461,RaviTeja,Reshma,Jayanth,Josthna,
+        }
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Roll numbers of the candidates are:");
+            foreach(int i in Enum.GetValues(typeof(ECE)))
+            {
+                Console.WriteLine($"{Enum.GetName(typeof(ECE),i)} : "+i);
+            }
+            
+            
+        }
+    }
+}
